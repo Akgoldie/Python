6baaa25ff06188ae14675ed55d1289a4a7865ed9
Arun Kumar <112628113+Akgoldie@users.noreply.github.com>
2023-01-19 22:51:13 +0530

    Add files via upload

---
+++ Bulid-in type.py
@@ -0,0 +1,21 @@
+a = 2
+print(type(a))
+
+b = 9223372036854775807
+print(type(b))
+
+pi = 3.14
+print(type(pi))
+
+c = 'A'
+print(type(c))
+
+name = 'Arun kumar'
+print(type(name))
+
+q = True
+print(type(q))
+
+x = None
+print(type(x))
+
